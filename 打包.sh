48eadbc05b714af86cf65b42f3d0be2582b30285
AlwaysBusyLzc <2035941342@qq.com>
2025-05-23 11:54:53 +0800

    优化 打包.sh

--- 打包.sh
+++ 打包.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-# 创建虚拟环境
-python3 -m venv .venv
-source .venv/bin/activate
+#apt update -y
+#apt install python3.10-venv
+#
+## 创建虚拟环境
+#python3 -m venv .venv
+#source .venv/bin/activate
+#
+## 设置阿里云镜像源
+#cat << EOF > .venv/pip/pip.conf
+#[global]
+#index-url = https://mirrors.aliyun.com/pypi/simple
+#trusted-host = mirrors.aliyun.com
+#EOF
 
-# 设置阿里云镜像源
-cat << EOF > .venv/pip/pip.conf
-[global]
-index-url = https://mirrors.aliyun.com/pypi/simple
-trusted-host = mirrors.aliyun.com
-EOF
+
 
 # 安装依赖
-pip install -r requirements.txt
+#pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://pypi.douban.com/simple
+pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
+#pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
 
 # 安装PyInstaller
 pip install pyinstaller
