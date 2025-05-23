1db918d7d47fafe606c929702c9c4df48fa6e5b0
AlwaysBusyLzc <2035941342@qq.com>
2025-05-23 14:34:33 +0800

    增加windows 打包 批处理

---
+++ build.bat
@@ -0,0 +1,15 @@
+@echo off
+
+rem 安装依赖
+rem pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://pypi.douban.com/simple
+pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://mirrors.aliyun.com/pypi/simple
+rem  pip install --progress-bar off --no-cache-dir -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+
+rem 安装PyInstaller
+pip install pyinstaller
+
+rem pyinstaller --onefile --add-data ".env:." main.py
+pyinstaller --name svc_ids --onefile main.py
+rem  拷贝 .env
+copy .env dist\
